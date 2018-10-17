5f09433ba3492f617409da41e1edcfacc0b6c78b
Marko Cupic <m.cupic@gmx.ch>
2018-10-17 22:55:44 +0200

    dev

--- src/Resources/contao/forms/FormMultiText.php
+++ src/Resources/contao/forms/FormMultiText.php
@@ -16,7 +16,7 @@ namespace Contao;
  * @property integer $mSize
  * @property boolean $mandatory
  * @property boolean $multiple
- * @property array   $options
+ * @property array $options
  * @property boolean $chosen
  *
  * @author Leo Feyer <https://github.com/leofeyer>
@@ -24,149 +24,150 @@ namespace Contao;
 class FormMultiText extends \Widget
 {
 
-	/**
-	 * Submit user input
-	 *
-	 * @var boolean
-	 */
-	protected $blnSubmitInput = true;
+    /**
+     * Submit user input
+     *
+     * @var boolean
+     */
+    protected $blnSubmitInput = true;
 
-	/**
-	 * Add a for attribute
-	 *
-	 * @var boolean
-	 */
-	protected $blnForAttribute = true;
+    /**
+     * Add a for attribute
+     *
+     * @var boolean
+     */
+    protected $blnForAttribute = true;
 
-	/**
-	 * Template
-	 *
-	 * @var string
-	 */
-	protected $strTemplate = 'form_multi_text';
+    /**
+     * Template
+     *
+     * @var string
+     */
+    protected $strTemplate = 'form_multi_text';
 
-	/**
-	 * The CSS class prefix
-	 *
-	 * @var string
-	 */
-	protected $strPrefix = 'widget widget-multi-text';
+    /**
+     * The CSS class prefix
+     *
+     * @var string
+     */
+    protected $strPrefix = 'widget widget-multi-text';
 
-	/**
-	 * Add specific attributes
-	 *
-	 * @param string $strKey   The attribute name
-	 * @param mixed  $varValue The attribute value
-	 */
-	public function __set($strKey, $varValue)
-	{
+    /**
+     * Add specific attributes
+     *
+     * @param string $strKey The attribute name
+     * @param mixed $varValue The attribute value
+     */
+    public function __set($strKey, $varValue)
+    {
         switch ($strKey)
-		{
+        {
             case 'varValue':
-		    case 'value':
-		        $this->varValue = \StringUtil::deserialize($varValue);
+            case 'value':
+                $this->varValue = \StringUtil::deserialize($varValue);
                 $this->arrOptions = \StringUtil::deserialize($varValue);
                 break;
-            break;
+                break;
             case 'mandatory':
-				if ($varValue)
-				{
-					$this->arrAttributes['required'] = 'required';
-				}
-				else
-				{
-					unset($this->arrAttributes['required']);
-				}
-				parent::__set($strKey, $varValue);
-				break;
+                if ($varValue)
+                {
+                    $this->arrAttributes['required'] = 'required';
+                }
+                else
+                {
+                    unset($this->arrAttributes['required']);
+                }
+                parent::__set($strKey, $varValue);
+                break;
 
-			case 'mSize':
-				// Ignore
-				break;
+            case 'mSize':
+                // Ignore
+                break;
 
-			case 'multiple':
-				// Ignore
-				break;
+            case 'multiple':
+                // Ignore
+                break;
 
-			case 'options':
-				$this->arrOptions = \StringUtil::deserialize($varValue);
-				break;
+            case 'options':
+                $this->arrOptions = \StringUtil::deserialize($varValue);
+                break;
 
-			case 'rgxp':
-			case 'minlength':
-			case 'maxlength':
-				// Ignore
-				break;
+            case 'rgxp':
+            case 'minlength':
+            case 'maxlength':
+                // Ignore
+                break;
 
-			default:
-				parent::__set($strKey, $varValue);
-				break;
-		}
-	}
+            default:
+                parent::__set($strKey, $varValue);
+                break;
+        }
+    }
 
-	/**
-	 * Check options if the field is mandatory
-	 */
-	public function validate()
-	{
-	    $mandatory = $this->mandatory;
-		$options = $this->getPost($this->strName);
+    /**
+     * Check options if the field is mandatory
+     */
+    public function validate()
+    {
+        $mandatory = $this->mandatory;
+        $options = $this->getPost($this->strName);
 
-		// Remove empty values
-		$options = array_filter($options);
+        // Remove empty values
+        $options = array_filter($options);
 
-		// Check if there is at least one value
-		if ($mandatory && \is_array($options))
-		{
-			foreach ($options as $option)
-			{
-				if (\strlen($option))
-				{
-					$this->mandatory = false;
-					break;
-				}
-			}
-		}
+        // Check if there is at least one value
+        if ($mandatory && \is_array($options))
+        {
+            foreach ($options as $option)
+            {
+                if (\strlen($option))
+                {
+                    $this->mandatory = false;
+                    break;
+                }
+            }
+        }
 
-		$varInput = $this->validator($options);
+        $varInput = $this->validator($options);
 
-		// Check for a valid option (see #4383)
-		if (!empty($varInput) && !$this->isValidOption($varInput))
-		{
-			//$this->addError($GLOBALS['TL_LANG']['ERR']['invalid']);
-		}
+        // Check for a valid option (see #4383)
+        if (!empty($varInput) && !$this->isValidOption($varInput))
+        {
+            //$this->addError($GLOBALS['TL_LANG']['ERR']['invalid']);
+        }
 
-		// Add class "error"
-		if ($this->hasErrors())
-		{
-			$this->class = 'error';
-		}
-		else
-		{
-			$this->varValue = $varInput;
-			$this->options = $varInput;
-		}
+        // Add class "error"
+        if ($this->hasErrors())
+        {
+            $this->class = 'error';
+        }
+        else
+        {
+            $this->varValue = $varInput;
+            $this->options = $varInput;
+        }
 
-		// Reset the property
-		if ($mandatory)
-		{
-			$this->mandatory = true;
-		}
-	}
+        // Reset the property
+        if ($mandatory)
+        {
+            $this->mandatory = true;
+        }
+    }
 
-	/**
-	 * Return a parameter
-	 *
-	 * @param string $strKey The parameter name
-	 *
-	 * @return mixed The parameter value
-	 */
-	public function __get($strKey){
+    /**
+     * Return a parameter
+     *
+     * @param string $strKey The parameter name
+     *
+     * @return mixed The parameter value
+     */
+    public function __get($strKey)
+    {
 
-		if ($strKey == 'options')
-		{
-			return $this->arrOptions;
-		}
+        if ($strKey == 'options')
+        {
+            return $this->arrOptions;
+        }
         if ($strKey == 'value')
         {
             return $this->arrOptions;
@@ -175,94 +176,92 @@ class FormMultiText extends \Widget
         {
             return $this->arrOptions;
         }
-		return parent::__get($strKey);
-	}
+        return parent::__get($strKey);
+    }
 
-	/**
-	 * Parse the template file and return it as string
-	 *
-	 * @param array $arrAttributes An optional attributes array
-	 *
-	 * @return string The template markup
-	 */
-	public function parse($arrAttributes=null)
-	{
-		$strClass = 'multi-text';
+    /**
+     * Parse the template file and return it as string
+     *
+     * @param array $arrAttributes An optional attributes array
+     *
+     * @return string The template markup
+     */
+    public function parse($arrAttributes = null)
+    {
+        $strClass = 'multi-text';
 
-		// Custom class
-		if ($this->strClass != '')
-		{
-			$strClass .= ' ' . $this->strClass;
-		}
+        // Custom class
+        if ($this->strClass != '')
+        {
+            $strClass .= ' ' . $this->strClass;
+        }
 
-		$this->strClass = $strClass;
+        $this->strClass = $strClass;
 
-		return parent::parse($arrAttributes);
-	}
+        return parent::parse($arrAttributes);
+    }
 
-	/**
-	 * Generate the options
-	 *
-	 * @return array The options array
-	 */
-	protected function getOptions()
-	{
-		$arrOptions = array();
-		$blnHasGroups = false;
+    /**
+     * Generate the options
+     *
+     * @return array The options array
+     */
+    protected function getOptions()
+    {
+        $arrOptions = array();
+        $blnHasGroups = false;
 
-		// Add empty option if there are none
-		if (empty($this->arrOptions) || !\is_array($this->arrOptions))
-		{
-			$this->arrOptions = array('');
-		}
+        // Add empty option if there are none
+        if (empty($this->arrOptions) || !\is_array($this->arrOptions))
+        {
+            $this->arrOptions = array('');
+        }
 
-		// Generate options
-		foreach ($this->arrOptions as $value)
-		{
-		    $arrOptions[] = array
-					(
-						'type'     => 'option',
-						'value'    => $value,
-					);
-		}
-
-
-
-		return $arrOptions;
-	}
-
-	/**
-	 * Generate the widget and return it as string
-	 *
-	 * @return string The widget markup
-	 */
-	public function generate()
-	{
-		$strOptions = '';
-
-		// Add empty option if there are none
-		if (empty($this->arrOptions) || !\is_array($this->arrOptions))
-		{
-			$this->arrOptions = array(array('type' => 'option','value'=>''));
-		}
-
-		foreach ($this->arrOptions as $arrOption)
-		{
+        // Generate options
+        foreach ($this->arrOptions as $value)
+        {
+            $arrOptions[] = array
+            (
+                'type'  => 'option',
+                'value' => $value,
+            );
+        }
 
 
-			$strOptions .= sprintf('<option value="%s"%s>%s</option>',
-									$arrOption['value'],
-									$this->isSelected($arrOption),
-									$arrOption['label']);
-		}
+        return $arrOptions;
+    }
+
+    /**
+     * Generate the widget and return it as string
+     *
+     * @return string The widget markup
+     */
+    public function generate()
+    {
+        $strOptions = '';
+
+        // Add empty option if there are none
+        if (empty($this->arrOptions) || !\is_array($this->arrOptions))
+        {
+            $this->arrOptions = array(array('type' => 'option', 'value' => ''));
+        }
+
+        foreach ($this->arrOptions as $arrOption)
+        {
 
 
+            $strOptions .= sprintf('<option value="%s"%s>%s</option>',
+                $arrOption['value'],
+                $this->isSelected($arrOption),
+                $arrOption['label']);
+        }
 
-		return sprintf('<select name="%s" id="ctrl_%s" class="%s"%s>%s</select>',
-						$this->strName,
-						$this->strId,
-						$this->class,
-						$this->getAttributes(),
-						$strOptions);
-	}
+
+        return sprintf('<select name="%s" id="ctrl_%s" class="%s"%s>%s</select>',
+            $this->strName,
+            $this->strId,
+            $this->class,
+            $this->getAttributes(),
+            $strOptions);
+    }
 }
--- src/Resources/contao/modules/PartnerFrontendForm.php
+++ src/Resources/contao/modules/PartnerFrontendForm.php
@@ -10,6 +10,7 @@ namespace Markocupic\FrankfurterPartnerBundle\Contao\Modules;
 
 use Contao\CcCardealerModel;
 use Contao\Controller;
+use Contao\CoreBundle\Exception\PageNotFoundException;
 use Contao\CoreBundle\Monolog\ContaoContext;
 use Contao\Database;
 use Contao\Dbafs;
@@ -95,7 +96,6 @@ class PartnerFrontendForm extends Module
         }
 
 
-
         if (FE_USER_LOGGED_IN)
         {
             $this->objUser = FrontendUser::getInstance();
@@ -131,15 +131,131 @@ class PartnerFrontendForm extends Module
         $url = Environment::get('uri');
         $objForm->setFormActionFromUri($url);
 
-        $objModel= CcCardealerModel::findById(150);
-        $value =  $objModel->ffm_partner_open;
+        // Add hidden fields REQUEST_TOKEN & FORM_SUBMIT
+        $objForm->addContaoHiddenFields();
+
+        $objDb = Database::getInstance()->prepare('SELECT * FROM cc_cardealer WHERE memberid=?')->limit(1)->execute($this->objUser->id);
+        if (!$objDb->numRows)
+        {
+            throw new PageNotFoundException('Page not found: ' . Environment::get('uri'));
+        }
+        $objModel = CcCardealerModel::findById($objDb->id);
+
 
         // Add some fields
+        $objForm->addFormField('name', array(
+            'label'     => 'Name',
+            'inputType' => 'text',
+            'value'     => $objModel->name
+        ));
+
+        $objForm->addFormField('alias', array(
+            'label'     => 'Alias',
+            'inputType' => 'text',
+            'value'     => $objModel->alias
+        ));
+
+        /**
+         * $objForm->addFormField('ffm_partner_cat', array(
+         * 'label'     => 'Kategorie',
+         * 'inputType' => 'text',
+         * 'value'     => $objModel->ffm_partner_cat
+         * ));
+         **/
+
+        $objForm->addFormField('ffm_partner_filiale', array(
+            'label'     => 'Filiale',
+            'inputType' => 'text',
+            'value'     => $objModel->ffm_partner_filiale
+        ));
+
+        $objForm->addFormField('ffm_partner_strasse', array(
+            'label'     => 'Strasse / Nr',
+            'inputType' => 'text',
+            'value'     => $objModel->ffm_partner_strasse
+        ));
+
+        $objForm->addFormField('ffm_partner_plz', array(
+            'label'     => 'PLZ',
+            'inputType' => 'text',
+            'value'     => $objModel->ffm_partner_plz
+        ));
+
+        $objForm->addFormField('ffm_partner_ort', array(
+            'label'     => 'Ort',
+            'inputType' => 'text',
+            'value'     => $objModel->ffm_partner_ort
+        ));
+
         $objForm->addFormField('ffm_partner_open', array(
             'label'     => 'Öffnungszeiten',
             'inputType' => 'formMultiText',
-            'value' => $value
+            'value'     => $objModel->ffm_partner_open
         ));
+
+        $objForm->addFormField('ffm_partner_tel', array(
+            'label'     => 'Telefon',
+            'inputType' => 'text',
+            'value'     => $objModel->ffm_partner_tel
+        ));
+
+        $objForm->addFormField('ffm_partner_mail', array(
+            'label'     => 'E-Mail',
+            'inputType' => 'text',
+            'value'     => $objModel->ffm_partner_mail
+        ));
+
+        $objForm->addFormField('ffm_partner_www', array(
+            'label'     => 'Webseite',
+            'inputType' => 'text',
+            'value'     => $objModel->ffm_partner_www
+        ));
+
+        $objForm->addFormField('ffm_partner_www_linkText', array(
+            'label'     => 'Linktext (Webseite)',
+            'inputType' => 'text',
+            'value'     => $objModel->ffm_partner_www_linkText
+        ));
+
+        $objForm->addFormField('ffm_partner_facebook', array(
+            'label'     => 'Social Facebook',
+            'inputType' => 'text',
+            'value'     => $objModel->ffm_partner_facebook
+        ));
+
+        $objForm->addFormField('ffm_partner_twitter', array(
+            'label'     => 'Social Twitter',
+            'inputType' => 'text',
+            'value'     => $objModel->ffm_partner_twitter
+        ));
+
+        $objForm->addFormField('ffm_partner_instagram', array(
+            'label'     => 'Social Instagram',
+            'inputType' => 'text',
+            'value'     => $objModel->ffm_partner_instagram
+        ));
+
+        $objForm->addFormField('ffm_partner_google', array(
+            'label'     => 'Social Google Plus',
+            'inputType' => 'text',
+            'value'     => $objModel->ffm_partner_google
+        ));
+
+        $objForm->addFormField('ffm_partner_youtubeid', array(
+            'label'     => 'Youtube ID',
+            'inputType' => 'text',
+            'value'     => $objModel->ffm_partner_youtubeid
+        ));
+
+        $objForm->addFormField('ffm_partner_text', array(
+            'label'     => 'Beschreibung',
+            'inputType' => 'textarea',
+            'eval'      => array('preserveTags' => true, 'allowHtml' => true, 'decodeEntities' => true),
+            'value'     => StringUtil::decodeEntities($objModel->ffm_partner_text)
+        ));
+
+
+        //die(print_r($objModel->row(),true));
 
 
         // Let's add  a submit button
@@ -156,28 +272,23 @@ class PartnerFrontendForm extends Module
         //$objWidgetFileupload->addAttribute('accept', '.jpg, .jpeg');
         //$objWidgetFileupload->storeFile = true;
 
-        if(Input::post('FORM_SUBMIT') != '')
+        if (Input::post('FORM_SUBMIT') != '')
         {
-            if($objForm->validate())
+            if ($objForm->validate())
             {
-                if(Input::post('ffm_partner_open') != '')
-                {
-                    //$objModel->ffm_partner_open = serialize(Input::post('ffm_partner_open'));
-                    $objModel->save();
-
-                }
+                // Beschreibung use tinyMce
+                // $objModel->ffm_partner_text = Input::postRaw('ffm_partner_text');
+                $objModel->save();
                 $this->reload();
-            }else{
+            }
+            else
+            {
+
             }
         }
 
-        $this->objForm = $objForm->generate();
+        $this->objForm = $objForm;
     }
-
-
-
-
-
 
 
 }
